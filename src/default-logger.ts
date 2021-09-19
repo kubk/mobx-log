55af4d40d8c6690f83b63df145078960c5ba3355
Egor Gorbachev <7gorbachevm@gmail.com>
2021-09-19 15:35:54 +0300

    Colorful output; shorten observable/computed/action

--- src/default-logger.ts
+++ src/default-logger.ts
@@ -2,12 +2,22 @@ import { ActionEvent, ComputedEvent, Logger, ObservableEvent } from './types';
 import { LogWriter } from './log-writer';
 import { Now } from './now';
 
+const colors = {
+  red: '#FF6157',
+  blue: '#28B7F6',
+  orange: '#FFBC2D',
+  green: '#2BC941',
+};
+
+const buildStyles = (color: string) => `color:${color}`;
+
 export class DefaultLogger implements Logger {
   constructor(private logWriter: LogWriter, private now?: Now) {}
 
   logObservable(event: ObservableEvent) {
     const info = [
-      'OBSERVABLE',
+      '%c[O]',
+      buildStyles(colors.orange),
       event.name,
       event.oldValue,
       '->',
@@ -22,7 +32,7 @@ export class DefaultLogger implements Logger {
   }
 
   logAction(event: ActionEvent) {
-    const info = ['ACTION', event.name, event.arguments];
+    const info = ['%c[A]', buildStyles(colors.red), event.name, event.arguments];
 
     if (this.now) {
       info.unshift(this.now());
@@ -32,7 +42,14 @@ export class DefaultLogger implements Logger {
   }
 
   logComputed(event: ComputedEvent) {
-    const info = ['COMPUTED', event.name, event.oldValue, '->', event.newValue];
+    const info = [
+      '%c[C]',
+      buildStyles(colors.blue),
+      event.name,
+      event.oldValue,
+      '->',
+      event.newValue,
+    ];
 
     if (this.now) {
       info.unshift(this.now());
